644e82bcf6576cbcf099f3276c7abcd100a0050a
iortego42 <iortego42madrid@gmail.com>
2022-11-05 22:11:40 +0100

    .env file

--- wherearethemonkeys.py
+++ wherearethemonkeys.py
@@ -1,8 +1,34 @@
 #!/usr/local/bin/python3
+import yaml
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+U_ID = getenv("UID")
+SECRET_ID = getenv("SECRETID")
+
+config_template = f"""
+---
+  intra:
+    client: "{U_ID}" #UID
+    secret: "{SECRET_ID}" #SECRET
+    uri: "https://api.intra.42.fr/v2/oauth/token"
+    endpoint: "https://api.intra.42.fr/v2"
+    scopes: ""
+"""
+
+config_template = yaml.safe_load(config_template)
+with open("config.yml", 'w') as config:
+    yaml.dump(config_template, config, explicit_start=True)
+
+
 from intra import ic
+
 
 with open("monkeys") as file:
     user_out = file.read().replace('\n', ',')
+
 payload = {
         "filter[login]": user_out
 }
