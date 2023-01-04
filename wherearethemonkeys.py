77c8af02da8f364d06b47d4a9a50cdee1fea2783
iortego42 <iortego42madrid@gmail.com>
2023-01-04 11:12:44 +0100

    New update terminal version adapted

--- wherearethemonkeys.py
+++ wherearethemonkeys.py
@@ -3,8 +3,13 @@ import yaml
 from os import getenv
 from dotenv import load_dotenv
 import sys
-from intra import ic
-
+#from wherearethemonkeys.intra import ic
+if __name__ == '__main__':
+    from intra import ic
+    config_file = "config.yml"
+else:
+    config_file = "../config.yml"
+    from wherearethemonkeys.intra import ic
 
 def charge_env() -> None:
     load_dotenv()
@@ -21,31 +26,36 @@ def charge_env() -> None:
     """
 
     config_template = yaml.safe_load(config_template)
-    with open("config.yml", 'w') as config:
+    with open(config_file, 'w') as config:
         yaml.dump(config_template, config, explicit_start=True)
 
 
 class Locator:
-    def __init__(self, users_input=""):
+    def __init__(self):
         charge_env()
         try:
             with open(sys.argv[1]) as file:
                 self.user_out = file.read().replace('\n', ',')
-        except(FileNotFoundError):
+        except (FileNotFoundError):
             print("\033[31mERROR\nFile not founded")
             exit()
         except (IndexError):
-            if users_input:
-                self.user_out = users_input
-            else:
-                print("\033[31mERROR\nNo users input given")
-                exit()
+            self.user_out = None
         self.payload = {
-            "filter[login]": self.user_out
+            "filter[login]": None
         }
         if not ic.client_id or not ic.client_secret:
-            print ("\033[31mERROR\nIncorrect API credentials")
+            print("\033[31mERROR\nIncorrect API credentials")
             exit()
+
+    def set_payload(self, users_input=""):
+        if users_input:
+            self.user_out = users_input
+        elif not self.user_out:
+            print("\033[31mERROR\nNo users input given")
+            exit()
+        
+        self.payload["filter[login]"] = self.user_out
 
     def shell_list(self) -> None:
         response = ic.pages_threaded("users", params=self.payload)
@@ -61,7 +71,7 @@ class Locator:
                 print(f"  - {user['login']}")
         print("\n")
 
-    def list(self) -> dict:
+    def dict_list(self) -> dict:
         response = ic.pages_threaded("users", params=self.payload)
         usersdict = {}
         for user in response:
@@ -69,6 +79,16 @@ class Locator:
         return usersdict
 
 
-locator = Locator()
-locator.shell_list()
-print(locator.list())
+if __name__ == "__main__":
+    locator = Locator()
+    locator.set_payload()
+    locator.shell_list()
+   # def give_format(list: dict) -> str:
+   #     location = "Here are your friends:\n"
+   #     for user in list:
+   #         if list[user]:
+   #             location += f"""
+   #             {user} -> {list[user]}
+   #             """
+   #     return location
+
